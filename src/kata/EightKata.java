e14b2b52f8681aaed2e63af993b0cec93633965d
Yuriy Shymkov <iuriyshymkov@gmail.com>
2021-11-26 19:35:24 +0200

    initialize project

---
+++ src/kata/EightKata.java
@@ -0,0 +1,4 @@
+package kata;
+public interface EightKata {
+    double getVolumeOfCuboid(final double length, final double width, final double height);
+}
---
+++ src/kata/impl/YuriyShymkov/EightKataImpl.java
@@ -0,0 +1,11 @@
+package kata.impl.YuriyShymkov;
+
+import kata.EightKata;
+
+public class EightKataImpl implements EightKata {
+
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+}
