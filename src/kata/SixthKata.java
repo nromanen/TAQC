e20b051bf891960e446b472e5f09611f038c2097
volodeead <volodeead.v@gmail.com>
2021-11-27 17:12:53 +0200

    SixthKatat

---
+++ src/kata/SixthKata.java
@@ -0,0 +1,19 @@
+package kata;
+
+public interface SixthKata {
+
+	long findNb(long m);
+
+	String balance(String book);
+
+	double f(double x);
+
+	double mean(String town, String strng);
+
+	double variance(String town, String strng);
+
+	String nbaCup(String resultSheet, String toFind);
+
+	String stockSummary(String[] lstOfArt, String[] lstOf1stLetter);
+
+}
