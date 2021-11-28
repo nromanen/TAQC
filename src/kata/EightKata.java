77e70fb3783065bcd97800bc5daf2478cb6fb37f
DESKTOP-52QONSF\HP <nastiaklass41062@gmail.com>
2021-11-28 18:14:48 +0200

    Added methods

--- src/kata/EightKata.java
+++ src/kata/EightKata.java
@@ -1,4 +1,21 @@
 package kata;
 public interface EightKata {
-    double getVolumeOfCuboid(final double length, final double width, final double height);
+    double getVolumeOfCuboid(final double length, final double width, final double height); // for Volume of a Cuboid task
+
+    int Liters(double time); // for Keep Hydrated! task
+
+    float mpgToKPM(final float mpg); // for Miles per gallon to kilometers per liter task
+
+    int[] squareOrSquareRoot(int[] array); // for To square(root) or not to square(root) task
+
+    int[] countPositivesSumNegatives(int[] input); // for Count of positives / sum of negatives task
+
+    int stringToNumber(String str); // for Convert a String to a Number! task
+
+    boolean am_i_wilson(double n); // for Wilson primes task
+
+    double TwoDecimalPlaces(double number); // for Formatting decimal places task
+
+    int[] divisibleBy(int[] numbers, int divider); // for Find numbers which are divisible by given number task
+
 }
