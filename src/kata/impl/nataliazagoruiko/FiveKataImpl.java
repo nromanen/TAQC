d031f01e148a8b3e70aa8ff1e0d7a47554800b13
natalia <nataliazag0610@gmail.com>
2021-11-28 23:38:48 +0200

    Created interface FiveKata and implementation;

---
+++ src/kata/impl/nataliazagoruiko/FiveKataImpl.java
@@ -0,0 +1,37 @@
+package kata.impl.nataliazagoruiko;
+
+import kata.FiveKata;
+
+import java.math.BigInteger;
+
+public class FiveKataImpl implements FiveKata {
+    @Override
+    public int artificialRain(int[] v) {
+        return 0;
+    }
+
+    @Override
+    public long[] gap(int g, long m, long n) {
+        return new long[0];
+    }
+
+    @Override
+    public int zeros(int n) {
+        return 0;
+    }
+
+    @Override
+    public BigInteger perimeter(BigInteger n) {
+        return null;
+    }
+
+    @Override
+    public double solve(double m) {
+        return 0;
+    }
+
+    @Override
+    public long[] smallest(long n) {
+        return new long[0];
+    }
+}
