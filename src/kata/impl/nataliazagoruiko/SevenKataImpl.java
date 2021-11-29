ff7c43c219da995a051911231d05efab0ac86a80
natalia <nataliazag0610@gmail.com>
2021-11-30 01:05:35 +0200

    Implemented the newAvg method of the SevenKata interface.

---
+++ src/kata/impl/nataliazagoruiko/SevenKataImpl.java
@@ -0,0 +1,38 @@
+package kata.impl.nataliazagoruiko;
+
+import kata.SevenKata;
+
+public class SevenKataImpl implements SevenKata {
+
+    /**
+     * The function returns the expected donation (rounded up to the next integer)
+     * that will permit to reach the average navg.
+     */
+    @Override
+    public long newAvg(double[] arr, double navg) {
+        double sum = 0;
+
+        int i = 0;
+        while (i < arr.length) {
+            sum += arr[i];
+            i++;
+        }
+
+        double lastDonat = navg * (arr.length + 1) - sum;
+        if (lastDonat < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return (long) Math.ceil(lastDonat);
+    }
+
+    @Override
+    public String seriesSum(int n) {
+        return null;
+    }
+
+    @Override
+    public int whereIsHe(int p, int bef, int aft) {
+        return 0;
+    }
+}
