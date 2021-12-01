eb1f54d415394e4e5a01127f93d22d33966f0f09
SHYMKOV <iuriyshymkov@gmail.com>
2021-12-01 11:23:50 +0200

    added java docs to eight kata interface

--- src/kata/EightKata.java
+++ src/kata/EightKata.java
@@ -1,71 +1,63 @@
 package kata;
 
 public interface EightKata {
-	
-	/**
-	 * for Volume of a Cuboid task
-	 * @param length of cuboid
-	 * @param width of cuboid
-	 * @param height of cuboid
-	 * @return volume of cuboid
-	 */
-    double getVolumeOfCuboid(final double length, final double width, final double height); 
 
-    /**
-     * for Keep Hydrated! task
-     * @param time
-     * @return
+  /** for Volume of a Cuboid task
+   * @param length of cuboid
+   * @param width of cuboid
+   * @param height of cuboid
+   * @return volume of cuboid
+   */
+    double getVolumeOfCuboid(final double length, final double width, final double height);
+
+    /** for Keep Hydrated! task
+     * @param time when Nathan drinks water
+     * @return the number of litres Nathan will drink
      */
-    int Liters(double time); 
+    int liters(double time);
 
-    /**
-     * for Miles per gallon to kilometers per liter task
-     * @param mpg
-     * @return
+    /** for Miles per gallon to kilometers per liter task
+     * @param mpg miles per gallon
+     * @return miles per gallon to kilometers
      */
-    float mpgToKPM(final float mpg); 
+    float mpgToKPM(final float mpg);
 
-    /**
-     * for To square(root) or not to square(root) task
-     * @param array
-     * @return
+    /** for To square(root) or not to square(root) task
+     * @param array of integer numbers
+     * @return an integer square root if the number has it, otherwise square the number.
      */
-    int[] squareOrSquareRoot(int[] array); 
+    int[] squareOrSquareRoot(int[] array);
 
-    /**
-     * for Count of positives / sum of negatives task
-     * @param input
-     * @return
+    /** for Count of positives / sum of negatives task
+     * @param input array of positives and negatives numbers
+     * @return count of positives / sum of negatives task
      */
-    int[] countPositivesSumNegatives(int[] input); 
+    int[] countPositivesSumNegatives(int[] input);
 
-    /**
-     * for Convert a String to a Number! task
-     * @param str
-     * @return
+    /** for Convert a String to a Number! task
+     * @param str given string
+     * @return converted string to a number
      */
-    double stringToNumber(String str);
+    int stringToNumber(String str);
 
-    /**
-     * for Wilson primes task
+    /** for Wilson primes task
      * @param n
      * @return
-     */
-    boolean am_i_wilson(double n);  
+             */
+    boolean amIWilson(double n);
 
-    /**
-     * for Formatting decimal places task
-     * @param number
-     * @return
+    /** for Formatting decimal places task
+     * @param number given double numbers
+     * @return formated numbers that are rounded to to decimal places
      */
-    double TwoDecimalPlaces(double number);  
+    double TwoDecimalPlaces(double number);
 
     /**
      * for Find numbers which are divisible by given number task
-     * @param numbers
-     * @param divider
-     * @return
+     * @param numbers array of integer numbers
+     * @param divider the number to be divided
+     * @return numbers which are divisible by given divider
      */
-    int[] divisibleBy(int[] numbers, int divider); 
+    int[] divisibleBy(int[] numbers, int divider);
 
 }
