30b61cc7b34e28a2f2fc02cab619c96fb73d6110
DESKTOP-52QONSF\HP <nastiaklass41062@gmail.com>
2021-12-07 14:34:32 +0200

    Created class Main and executed first and second method from EightKata, executed the fisrt method from SevenKata

--- src/kata/Main.java
+++ src/kata/Main.java
@@ -1,6 +1,7 @@
 package kata;
 
 import kata.impl.AnastasiaZadolinna.EightKataImpl;
+import kata.impl.DmytroHursrkyi.SevenKataImpl;
 
 import java.util.Scanner;
 
@@ -8,13 +9,72 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Enter your choice : \n 1 - to run getVolumeOfCuboid(double) method \n or other symbol to exit");
+        System.out.println("Choice kata:\n 1 - EightKata\n 2  - SevenKata\n  q - Exit");
 
         Scanner scanner = new Scanner(System.in);
 
-        int choice = Integer.parseInt(scanner.nextLine());
+        String choice = scanner.nextLine();
 
-        if (choice == 1) {
+        switch (choice) {
+
+            case "1":
+
+                eightKataHandler(scanner);
+
+                break;
+
+            case "2":
+
+                sevenKataHandle(scanner);
+
+                break;
+
+        }
+
+    }
+
+    private static void sevenKataHandle(Scanner scanner) {
+
+        String choice;
+
+        System.out.println("Choice your method: \n 1 - to run newAvg(double[], double)\n 2 -seriesSum(long)");
+
+        choice = scanner.nextLine();
+
+        if ("1".equals(choice)) {
+
+            System.out.println("Input double values separated by commas");
+
+            String arr = scanner.nextLine();
+
+            String[] convertedRankArray = arr.split("\\s*,\\s*");
+
+            double[] doubleArr = new double[convertedRankArray.length];
+
+            for (int i = 0; i < doubleArr.length; i++) {
+
+                doubleArr[i] = Double.parseDouble(convertedRankArray[i]);
+
+            }
+
+            System.out.println("Input double value");
+
+            double value = scanner.nextDouble();
+
+            System.out.println("The result of execute of newAvg method is: " + new SevenKataImpl().newAvg(doubleArr, value));
+
+        }
+
+    }
+
+    private static void eightKataHandler(Scanner scanner) {
+
+        System.out.println("Enter your choice : \n 1 - to run getVolumeOfCuboid(double,double,double) method \n " +
+                "2 - to run liters (double)\n 3 - to run mpgToKPM( float ) or other symbol to exit");
+
+        String choice = scanner.nextLine();
+
+        if ("1".equals(choice)) {
 
             System.out.println("Input double length:");
 
@@ -29,6 +89,16 @@ public class Main {
             Double height = Double.parseDouble(scanner.nextLine());
 
             System.out.println("The result of execute of getVolumeOfCuboid method is: " + new EightKataImpl().getVolumeOfCuboid(length, width, height));
+
+        } else if ("2".equals(choice)) {
+
+            System.out.println("Enter double number");
+
+            double value = scanner.nextDouble();
+
+            System.out.println("The result of execute of Liters method is" + new EightKataImpl().liters(value));
+
+            new EightKataImpl().liters(value);
 
         }
 
