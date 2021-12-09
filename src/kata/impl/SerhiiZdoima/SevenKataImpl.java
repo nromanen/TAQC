94c64968807258f60e989254cef4d1d0e266a75b
US18095196754\Admin555 <snakedrummm@gmail.com>
2021-12-09 16:27:17 +0200

    implement interface in 5,6 and 7 kata

---
+++ src/kata/impl/SerhiiZdoima/SevenKataImpl.java
@@ -0,0 +1,34 @@
+package kata.impl.SerhiiZdoima;
+
+import kata.SevenKata;
+
+import java.util.stream.DoubleStream;
+
+public class SevenKataImpl implements SevenKata {
+    @Override
+    public  long newAvg(double[] arr, double navg)
+    {
+        double ans = navg * (arr.length + 1) - DoubleStream.of(arr).sum();
+        if (ans < 0) {
+            throw new IllegalArgumentException();
+        }
+        return (long) (Math.ceil(ans));
+    }
+
+
+@Override
+public  String seriesSum(int n) {
+    {
+        Double sum = 0.00;
+        if (n == 0) {
+            return String.valueOf(sum);
+        } else {
+            for (Integer i = 0; i < n; i++) {
+                sum += Double.valueOf(1) / Double.valueOf(1 + i * 3);
+            }
+            return String.format("%.2f", sum);
+        }
+    }
+}
+}
+
