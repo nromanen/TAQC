b3028e27d9c4f5bb239c65a110baf81cabbad145
DimaHursky <77124180+DimaHursky@users.noreply.github.com>
2021-12-13 21:49:12 +0200

    refactoring of the main class, adding methods to СonsoleInlut method

--- src/kata/utils/ConsoleInput.java
+++ src/kata/utils/ConsoleInput.java
@@ -1,5 +1,23 @@
 package kata.utils;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ConsoleInput {
-    public static int intInput (){return 0;}
-}
+    public static int intInput (String welcomeMessage){
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println(welcomeMessage);
+            try {
+                return Integer.parseInt(input.nextLine());
+
+            }
+            catch(NumberFormatException | InputMismatchException e){
+                System.out.println("You should enter the integer value");
+            }
+        }
+        }
+      //  int arrayLenght = input.nextInt();
+    }
+
