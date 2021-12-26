19c5ad23b33ecb4810ff1132d25e91d24f0b79b4
DimaHursky <77124180+DimaHursky@users.noreply.github.com>
2021-12-26 22:52:45 +0200

    aded sevenKataTest end chanhes to tests

--- src/kata/impl/SerhiiZdoima/SevenKataImpl.java
+++ src/kata/impl/SerhiiZdoima/SevenKataImpl.java
@@ -6,8 +6,8 @@ import java.util.stream.DoubleStream;
 
 public class SevenKataImpl implements SevenKata {
     @Override
-    public  long newAvg(double[] arr, double navg)
-    {
+    public long newAvg(double[] arr, double navg) {
+        if (arr[1] < 0)throw new IllegalArgumentException("some message");
         double ans = navg * (arr.length + 1) - DoubleStream.of(arr).sum();
         if (ans < 0) {
             throw new IllegalArgumentException();
@@ -16,19 +16,19 @@ public class SevenKataImpl implements SevenKata {
     }
 
 
-@Override
-public  String seriesSum(int n) {
-    {
-        Double sum = 0.00;
-        if (n == 0) {
-            return String.valueOf(sum);
-        } else {
-            for (Integer i = 0; i < n; i++) {
-                sum += Double.valueOf(1) / Double.valueOf(1 + i * 3);
+    @Override
+    public String seriesSum(int n) {
+        {
+            Double sum = 0.00;
+            if (n == 0) {
+                return String.valueOf(sum);
+            } else {
+                for (Integer i = 0; i < n; i++) {
+                    sum += Double.valueOf(1) / Double.valueOf(1 + i * 3);
+                }
+                return String.format("%.2f", sum);
             }
-            return String.format("%.2f", sum);
         }
     }
-}
 }
 
---
+++ src/test/kata/SevenKataTest.java
@@ -0,0 +1,61 @@
+package test.kata;
+
+import kata.SevenKata;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SevenKataTest {
+
+    public static final double EPS = 0.001;
+
+    // @ParameterizedTest
+
+    //@MethodSource("newAvg") //до чого прив'язувати ім'я
+    @Test
+
+    public void lookingForABenefactorTest() {
+        double array1[] = new double[]{14.0, 30.0, 5.0, 7.0, 9.0, 11.0, 16.0};
+        int int1 = 90;
+        long actual = new kata.impl.SerhiiZdoima.SevenKataImpl().newAvg(array1, int1);
+        long expected = 628;
+        assertEquals(expected, actual);
+
+        array1[6] = 15.0;
+        int1 = 30;
+        expected = 149;
+        actual = new kata.impl.SerhiiZdoima.SevenKataImpl().newAvg(array1, int1); //newAv from array1 and int1 then the result == 628
+        assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("newAvgData")
+    public void newAvgTest(double[] arrData, int intData, long expected) {
+        long actual = new kata.impl.SerhiiZdoima.SevenKataImpl().newAvg(arrData, intData); //newAv from array1 and int1 then the result == 628
+        assertEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> newAvgData() {
+        return Stream.of(
+                Arguments.of(new double[]{14.0, 30.0, 5.0, 7.0, 9.0, 11.0, 16.0}, 90, 628),
+                Arguments.of(new double[]{14.0, 30.0, 5.0, 7.0, 9.0, 11.0, 15.0}, 30, 149)
+                //    Arguments.of(new kata.impl.DmytroHursrkyi.EightKataImpl())
+        );
+    }
+
+    @Test
+    void testExpectedException() {
+
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new kata.impl.SerhiiZdoima.SevenKataImpl().newAvg (new double[]{2, -5}, 2);
+        });
+
+        Assertions.assertEquals("some message", exception.getMessage());
+    }
+}
